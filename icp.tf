6e4b0ee1862a4e2927eb36b0310ed333ad48c524
garrettux <garrettux@gmail.com>
2018-01-09 11:23:39 -0500

    fix whitespace

--- icp.tf
+++ icp.tf
@@ -3,7 +3,7 @@
 resource "tls_private_key" "icpkey" {
   count       = "${var.generate_key ? 1 : 0}"
   algorithm   = "RSA"
-  
+
   provisioner "local-exec" {
     command = "cat > privatekey.pem <<EOL\n${tls_private_key.icpkey.private_key_pem}\nEOL"
   }
@@ -13,13 +13,13 @@ resource "tls_private_key" "icpkey" {
 resource "null_resource" "icp-cluster" {
 
   count = "${var.cluster_size}"
-  
+
   connection {
       host = "${element(local.icp-ips, count.index)}"
       user = "${var.ssh_user}"
       private_key = "${file(var.ssh_key)}"
   }
-   
+
   # Validate we can do passwordless sudo in case we are not root
   provisioner "remote-exec" {
     inline = [
@@ -30,9 +30,9 @@ resource "null_resource" "icp-cluster" {
   provisioner "file" {
       content = "${var.generate_key ? tls_private_key.icpkey.public_key_openssh : file(var.icp_pub_keyfile)}"
       destination = "/tmp/icpkey"
-  
+
   }
-  
+
   provisioner "remote-exec" {
     inline = [
       "mkdir -p /tmp/icp-common-scripts"
@@ -66,9 +66,9 @@ resource "null_resource" "icp-boot" {
     host = "${element(var.icp-master, 0)}"
     user = "${var.ssh_user}"
     private_key = "${file(var.ssh_key)}"
-  } 
+  }
 
-  
+
   # If this is enterprise edition we'll need to copy the image file over and load it in local repository
   // We'll need to find another workaround while tf does not support count for this
   provisioner "file" {
@@ -76,7 +76,7 @@ resource "null_resource" "icp-boot" {
       source = "${var.enterprise-edition ? var.image_file : "/dev/null" }"
       destination = "/tmp/${basename(var.image_file)}"
   }
-  
+
 
   provisioner "remote-exec" {
     inline = [
@@ -87,13 +87,13 @@ resource "null_resource" "icp-boot" {
     source      = "${path.module}/scripts/boot-master/"
     destination = "/tmp/icp-bootmaster-scripts"
   }
-  
+
   # store config yaml if it was specified
   provisioner "file" {
     source       = "${var.icp_config_file}"
     destination = "/tmp/config.yaml"
   }
-  
+
   # JSON dump the contents of icp_configuration items
   provisioner "file" {
     content     = "${jsonencode(var.icp_configuration)}"
@@ -119,12 +119,12 @@ resource "null_resource" "icp-boot" {
       content = "${var.generate_key ? tls_private_key.icpkey.private_key_pem : file(var.icp_priv_keyfile)}"
       destination = "/opt/ibm/cluster/ssh_key"
   }
-  
+
   provisioner "file" {
     content = "${join(",", var.icp-worker)}"
     destination = "/opt/ibm/cluster/workerlist.txt"
   }
-  
+
   provisioner "file" {
     content = "${join(",", var.icp-master)}"
     destination = "/opt/ibm/cluster/masterlist.txt"
@@ -133,37 +133,37 @@ resource "null_resource" "icp-boot" {
   provisioner "file" {
     content = "${join(",", var.icp-proxy)}"
     destination = "/opt/ibm/cluster/proxylist.txt"
-  }  
-  
+  }
+
   provisioner "remote-exec" {
     inline = [
       "/tmp/icp-bootmaster-scripts/generate_hostsfiles.sh",
       "/tmp/icp-bootmaster-scripts/start_install.sh ${var.icp-version}"
-      
+
     ]
   }
-  
-  # Check if var.ssh_user is root. If not add ansible_become lines 
-  
+
+  # Check if var.ssh_user is root. If not add ansible_become lines
+
   provisioner "remote-exec" {
     inline = [
-      
+
     ]
   }
 }
 
 resource "null_resource" "icp-worker-scaler" {
   depends_on = ["null_resource.icp-cluster", "null_resource.icp-boot"]
-  
+
   triggers {
     workers = "${join(",", var.icp-worker)}"
   }
-  
+
   connection {
     host = "${element(var.icp-master, 0)}"
     user = "${var.ssh_user}"
     private_key = "${file(var.ssh_key)}"
-  } 
+  }
 
   provisioner "file" {
     content = "${join(",", var.icp-worker)}"
@@ -174,14 +174,14 @@ resource "null_resource" "icp-worker-scaler" {
     source      = "${path.module}/scripts/boot-master/scaleworkers.sh"
     destination = "/tmp/icp-bootmaster-scripts/scaleworkers.sh"
   }
-  
+
   provisioner "remote-exec" {
     inline = [
       "chmod a+x /tmp/icp-bootmaster-scripts/scaleworkers.sh",
       "/tmp/icp-bootmaster-scripts/scaleworkers.sh ${var.icp-version}"
     ]
   }
-    
+
 
 
 }
