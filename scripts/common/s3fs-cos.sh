f1c6fde1542fcd591d414b5e6734d4165240c662
garrettux <garrettux@gmail.com>
2018-02-19 18:00:50 -0500

    moar checky

--- scripts/common/s3fs-cos.sh
+++ scripts/common/s3fs-cos.sh
@@ -11,7 +11,7 @@ install() {
   cd /tmp
   apt-get -y -q update
   apt-get -y -q install automake autotools-dev g++ git libcurl4-openssl-dev libfuse-dev libssl-dev libxml2-dev make pkg-config
-  git clone https://github.com/s3fs-fuse/s3fs-fuse.git
+  [[ -d /tmp/s3fs-fuse ]] || git clone https://github.com/s3fs-fuse/s3fs-fuse.git
   cd s3fs-fuse
   ./autogen.sh > /dev/null
   ./configure > /dev/null
